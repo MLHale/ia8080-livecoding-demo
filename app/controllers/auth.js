8b32161588340537130aa3dd3200e64763f9eed1
mlhale <mlhale@unomaha.edu>
2015-11-09 11:42:08 -0800

    fixes before integration with django

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -8,19 +8,17 @@ export default Ember.Controller.extend({
 	actions: {
 		login: function(){
 			//do stuff to authenticate here
-			console.log('login')
+			
 			var user = this.get('username');
-			if(user!="matt"){
-				this.set('errorMsg', 'youre not awesome');
+			console.log('login as: '+user);
+			if(user==""){
+				this.set('errorMsg', 'enter a username');
 			}
 			else{
 				this.set('loggedIn', true);
 				this.transitionTo('home');
 			}
 
-		},
-		test: function(){
-			console.log('test')
 		}
 	}
 });
