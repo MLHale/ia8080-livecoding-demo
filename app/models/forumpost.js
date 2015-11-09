a55ee58fc0b8c7e564ca2baba490e46095a26550
mlhale <mlhale@unomaha.edu>
2015-11-09 14:21:53 -0800

    app integrated with django rest and finishes step 16 during the writing of the tutorial

--- app/models/forumpost.js
+++ app/models/forumpost.js
@@ -1,8 +1,13 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
 	title: DS.attr('string'),
 	content: DS.attr('string'),
 	author: DS.attr('string'),
-	upvotes: DS.attr('number')
+	likes: DS.attr('number'),
+	upvotes: Ember.computed('likes', function(){
+		return this.get('likes');
+	}),
+	tags: DS.attr('array')
 });
